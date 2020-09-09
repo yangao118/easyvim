b5ef1df63843d0f12f1a739db9a28afaaa9c46c2
nobody <nobody@void.com>
2020-09-09 16:33:54 +0800

    add bootstrap.sh
    Signed-off-by: nobody <nobody@void.com>

---
+++ bootstrap.sh
@@ -0,0 +1,142 @@
+#!/usr/bin/env bash
+
+#   Based on spfvim-13's bootstrap.sh
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+############################  SETUP PARAMETERS
+[ -z "$APP_PATH" ] && APP_PATH=`pwd`
+app_name='easyvim'
+debug_mode='0'
+
+############################  BASIC SETUP TOOLS
+msg() {
+    printf '%b\n' "$1" >&2
+}
+
+success() {
+    if [ "$ret" -eq '0' ]; then
+        msg "\33[32m[✔]\33[0m ${1}${2}"
+    fi
+}
+
+error() {
+    msg "\33[31m[✘]\33[0m ${1}${2}"
+    exit 1
+}
+
+debug() {
+    if [ "$debug_mode" -eq '1' ] && [ "$ret" -gt '1' ]; then
+        msg "An error occurred in function \"${FUNCNAME[$i+1]}\" on line ${BASH_LINENO[$i+1]}, we're sorry for that."
+    fi
+}
+
+program_exists() {
+    local ret='0'
+    command -v $1 >/dev/null 2>&1 || { local ret='1'; }
+
+    # fail on non-zero return value
+    if [ "$ret" -ne 0 ]; then
+        return 1
+    fi
+
+    return 0
+}
+
+program_must_exist() {
+    program_exists $1
+
+    # throw error on non-zero return value
+    if [ "$?" -ne 0 ]; then
+        error "You must have '$1' installed to continue."
+    fi
+}
+
+variable_set() {
+    if [ -z "$1" ]; then
+        error "You must have your HOME environmental variable set to continue."
+    fi
+}
+
+lnif() {
+    if [ -e "$1" ]; then
+        ln -sf "$1" "$2"
+    fi
+    ret="$?"
+    debug
+}
+
+############################ SETUP FUNCTIONS
+do_backup() {
+    if [ -e "$1" ] || [ -e "$2" ] || [ -e "$3" ]; then
+        msg "Attempting to back up your original vim configuration."
+        today=`date +%Y%m%d_%s`
+        for i in "$1" "$2" "$3"; do
+            [ -e "$i" ] && [ ! -L "$i" ] && mv -v "$i" "$i.$today";
+        done
+        ret="$?"
+        success "Your original vim configuration has been backed up."
+        debug
+   fi
+}
+
+install_plugin_manager() {
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+}
+
+setup_plugin() {
+    local system_shell="$SHELL"
+    export SHELL='/bin/sh'
+
+    vim \
+        "+set nomore" \
+        "+PlugInstall" \
+        "+qall"
+
+    export SHELL="$system_shell"
+
+    success "Now updating/installing plugins using vim-plug"
+    debug
+}
+
+create_symlinks() {
+    local source_path="$1"
+    local target_path="$2"
+
+    lnif "$source_path/.vimrc"         "$target_path/.vimrc"
+    lnif "$source_path/.inputrc"        "$target_path/.inputrc"
+
+    ret="$?"
+    success "Setting up vim symlinks."
+    debug
+}
+
+############################ MAIN()
+variable_set "$HOME"
+program_must_exist "vim"
+program_must_exist "git"
+
+do_backup       "$HOME/.vim" \
+                "$HOME/.vimrc" \
+                "$HOME/.gvimrc"
+
+create_symlinks "$APP_PATH" \
+                "$HOME"
+
+install_plugin_manager
+
+setup_plugin
+
+msg             "\nThanks for installing $app_name."
+msg             "© `date +%Y` https://github.com/yangao118/easyvim"
